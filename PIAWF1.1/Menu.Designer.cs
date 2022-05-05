06927728840bd842e20fc4c1135cebaa4296f2e8
Jair Arturo Torres García <cylon0223@gmail.com>
2022-05-04 20:15:54 -0500

    Cambio nombre boton

--- PIAWF1.1/Menu.Designer.cs
+++ PIAWF1.1/Menu.Designer.cs
@@ -76,7 +76,7 @@
             this.btnPitcheo.Name = "btnPitcheo";
             this.btnPitcheo.Size = new System.Drawing.Size(91, 44);
             this.btnPitcheo.TabIndex = 3;
-            this.btnPitcheo.Text = "Estaditica de Pitcheo";
+            this.btnPitcheo.Text = "Estadistica de Pitcheo";
             this.btnPitcheo.UseVisualStyleBackColor = true;
             this.btnPitcheo.Click += new System.EventHandler(this.btnPitcheo_Click);
             // 
